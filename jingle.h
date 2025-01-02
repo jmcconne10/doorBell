cd8bcda5fe78c48a7ffc8edb9b0766406eecd5ee
EMcConnell20 <142705128+EMcConnell20@users.noreply.github.com>
2025-01-02 15:26:19 -0600

    Updated jingle.h [Songs, Documentation]

--- jingle.h
+++ jingle.h
@@ -5,7 +5,6 @@
 
 
 
-
 // —— Utility Stuff —— //
 
 
@@ -15,19 +14,19 @@
 #define COMPOSE(note_sheet, song_name, function_name, tempo, length) \
 const auto song_name = Song<tempo, length>(note_sheet); \
 void function_name() { \
-  for (int i = 0; i < length; ++i) { \
-    if (song_name.notes[i].pitch != 0.0) { tone(AUDIO_PIN, song_name.notes[i].pitch, song_name.notes[i].len); } \
-    delay(song_name.notes[i].len); \
-  } \
+	for (int i = 0; i < length; ++i) { \
+		if (song_name.notes[i].pitch != 0.0) { tone(AUDIO_PIN, song_name.notes[i].pitch, song_name.notes[i].len); } \
+		delay(song_name.notes[i].len); \
+	} \
 }
 
 #define COMPOSE_SHIFTED(note_sheet, song_name, function_name, tempo, length, octave_shift) \
 const auto song_name = Song<tempo, length>(note_sheet, octave_shift); \
 void function_name() { \
-  for (int i = 0; i < length; ++i) { \
-    if (song_name.notes[i].pitch != 0.0) { tone(AUDIO_PIN, song_name.notes[i].pitch, song_name.notes[i].len); } \
-    delay(song_name.notes[i].len); \
-  } \
+	for (int i = 0; i < length; ++i) { \
+		if (song_name.notes[i].pitch != 0.0) { tone(AUDIO_PIN, song_name.notes[i].pitch, song_name.notes[i].len); } \
+		delay(song_name.notes[i].len); \
+	} \
 }
 
 const double pow2(unsigned int n) { return n == 0 ? 1.0 : 2.0 * pow2(n - 1); }
@@ -99,99 +98,93 @@ Also, F is actually `F_` because the `F` function identifier is already taken by
 
 /// Stores the pitch and length of a single note.
 template <unsigned int TEMPO> struct Note {
-    unsigned int pitch; // Measured in Hz
-    unsigned int len; // Measured in ms.
-    
-    // Default Constructor
-    Note() : pitch(0), len(0) {} 
-    
-    // This turns the value produced from the note macros into an actual note.
-    Note(const double vals[2]) : pitch(static_cast<unsigned int>(vals[0])), len(static_cast<unsigned int>(4.0 * 60.0 * 1000.0 / static_cast<double>(TEMPO) / vals[1])) {}
+	unsigned int pitch; // Measured in Hz
+	unsigned int len; // Measured in ms.
 
-    // Same thing, but for Octave shifts.
-    Note(const double pitch, const double len) : pitch(static_cast<unsigned int>(pitch)), len(static_cast<unsigned int>(4.0 * 60.0 * 1000.0 / static_cast<double>(TEMPO) / len)) {}
+	// Default Constructor
+	Note() : pitch(0), len(0) {} 
+
+	// This turns the value produced from the note macros into an actual note.
+	Note(const double vals[2]) : pitch(static_cast<unsigned int>(vals[0])), len(static_cast<unsigned int>(4.0 * 60.0 * 1000.0 / static_cast<double>(TEMPO) / vals[1])) {}
+
+	// Same thing, but for Octave shifts.
+	Note(const double pitch, const double len) : pitch(static_cast<unsigned int>(pitch)), len(static_cast<unsigned int>(4.0 * 60.0 * 1000.0 / static_cast<double>(TEMPO) / len)) {}
 };
 
 /// Stores a list of `Note`s.
 template <unsigned int TEMPO, unsigned int LENGTH> struct Song {
-    Note<TEMPO> notes[LENGTH]; // The notes.
-    
-    // This converts a list of note macros into a list of `Note`s.
-    Song(const double vals[LENGTH][2]) { for(int i = 0; i < LENGTH; ++i) { notes[i] = Note<TEMPO>(vals[i]); } }
+	Note<TEMPO> notes[LENGTH]; // The notes.
 
-    // Use this to shift the song up and down octaves.
-    Song(const double vals[LENGTH][2], const int octave_shift) {
-      const double shift = octave_shift >= 0 ? pow2(static_cast<unsigned int>(octave_shift)) : 1.0 / pow2(static_cast<unsigned int>(-octave_shift));
+	// This converts a list of note macros into a list of `Note`s.
+	Song(const double vals[LENGTH][2]) { for(int i = 0; i < LENGTH; ++i) { notes[i] = Note<TEMPO>(vals[i]); } }
 
-      for(int i = 0; i < LENGTH; ++i) {
-        notes[i] = Note<TEMPO>(vals[i][0] * shift, vals[i][1]);
-      }
-    }
+	// Use this to shift the song up and down octaves.
+	Song(const double vals[LENGTH][2], const int octave_shift) {
+		const double shift = octave_shift >= 0 ? pow2(static_cast<unsigned int>(octave_shift)) : 1.0 / pow2(static_cast<unsigned int>(-octave_shift));
+
+	  	for(int i = 0; i < LENGTH; ++i) {
+			notes[i] = Note<TEMPO>(vals[i][0] * shift, vals[i][1]);
+		}
+	}
 };
 
 
 
-// —— Examples —— //
+// —— User Guide —— //
 
 
 
-/*
-## How To Make A Song ##
+/* ## How To Play A Song ##
 
-1. Define an array like "const double song_notes[song_length][2]".
-  * "song_notes" won't be the actual name of your song (that'll come up later). 
-  * "song_length" will be the number of notes in the song (you can do that at the end of step #2).
+1. Go down to the `Song Listing
 
-2. Fill the array with note macros.
-  * If you don't know what note macros are, go back up to the "Note Macros" section.
-  * Note macros will be played in the order they're listed.
-  * Rests cound as notes, so don't forget to count them too.
-
-3. Use the `COMPOSE` macro to create the song.
-  * The first argument should be the name of the array of note macros.
-  * The second argument should be the name of the song.
-  * The third argument should be what you want the play function to be named.
-  * The fourth argument should be the tempo of the song.
-  * The fifth argument should be the length of the song.
-
-P.S.
-You have to make a variable for the note macros; you can't just pass the list as an argument.
-I'm not a fan of this, but it's the best solution I could come up with. At least the
-syntax looks clean. The play function thing is just dumb though.
 */
 
-// Steps 1 & 2
+/* ## How To Add A Song ##
+
+1. Define an array like this: "const double song_notes[song_length][2]".
+	* "song_notes" won't be the actual name of your song (that'll come up later). 
+	* "song_length" will be the number of notes in the song.
+  
+2. Fill the array with note macros.
+  	* If you don't know what note macros are, go back up to the "Note Macros" section.
+  	* Note macros will be played in the order they are listed.
+  	* Rests are also notes, so make sure to count them in your song length.
+
+3. Use the `COMPOSE` macro to create the song.
+	* The first argument should be the name of the array you made in step one.
+	* The second argument should be the variable name you want to use for the song.
+	* The third argument should be the name you want for the play function.
+	* The fourth argument should be the tempo of the song in BPM.
+	* The fifth argument should be the number of notes in the song.
+
+// Steps 1 & 2 //
+
 const double c_scale_notes[15][2] = {
-  C(4, 1), D(4, 2), E(4, 2), F_(4, 2), G(4, 2), A(4, 2), B(4, 2), C(5, 1),
-  B(4, 2), A(4, 2), G(4, 2), F_(4, 2), E(4, 2), D(4, 2), C(4, 1)
+	C(4, 1), D(4, 2), E(4, 2),
+ 	F_(4, 2), G(4, 2), A(4, 2), B(4, 2),
+	C(5, 1), B(4, 2), A(4, 2),
+	G(4, 2), F_(4, 2), E(4, 2), D(4, 2), C(4, 1)
 };
 
-// Step 3
+// Step 3 //
+
+ 		[Macro Notes]		  [Function Name]
 COMPOSE(c_scale_notes, C_Scale, play_c_scale, 60, 15)
+					 [Song Name]	
+*/
+
+
+// —— Song Listings —— //
 
 
 
-// —— Run-Time Stuff —— //
-
-
-// Pretty sure I need to remove this, so it can be included in main code
-// void setup() {
-//   play_vitality();
-//   play_vitality();
-// }
-
-// void loop() {}
-
-
-
-// —— Song Listing —— //
-
-
-
-//    —— Song ——   |   —— Note Sheet ——   |   —— Variable ——  |   —— Play Function ——
-// Vitality        | vitality_notes       | Vitality          | play_vitality()
-// Industry Baby   | industry_baby_notes  | Industry_Baby     | play_industry_baby()
-
+//    ———— Song ————   |   ——— Macro Sheets ———   |   ——— Variables ———   |   ——— Play Function ———
+// Vitality            | vitality_notes           | Vitality              | play_vitality()
+// Industry Baby       | industry_baby_notes      | Industry_Baby         | play_industry_baby()
+// Bad Apple!!         | bad_apple_notes          | Bad_Apple             | play_bad_apple()
+// Bad Apple!! (Intro) | bad_apple_intro_notes    | Bad_Apple_Intro       | play_bad_apple_intro()
+// Bad Apple!! (Bridge)| bad_apple_bridge_notes   | Bad_Apple_Bridge      | play_bad_apple_bridge()
 
 /// Todo List
 
@@ -199,7 +192,6 @@ COMPOSE(c_scale_notes, C_Scale, play_c_scale, 60, 15)
 // The Grinch
 // Through The Fire And Flames
 // 9mm
-// Bad Apple!!
 // Smooth Criminal
 // Come A Little Bit Closer
 // The Game Theory Theme Song
@@ -210,51 +202,91 @@ COMPOSE(c_scale_notes, C_Scale, play_c_scale, 60, 15)
 // Your New Home
 
 const double vitality_notes[66][2] = {
-  C(4, 2), R(2), G(4, 2), Eb(4, 2), R(2), C(4, 2), R(2), C(5, 2), // 8 Notes
-  R(2), C(4, 2), R(2), Eb(4, 2), R(2), C(4, 2), D(4, 2), Eb(4, 2), // 8 Notes
-  G(4, 2), R(2), C(5, 2), D(5, 2), R(2), G(4, 2), R(2), B(4, 2), // 8 Notes
-  R(2), G(4, 2), R(2), Eb(4, 2), R(2), G(4, 2), F_(4, 2), Eb(4, 2), // 8 Notes
+	C(4, 2), R(2), G(4, 2), Eb(4, 2), R(2), C(4, 2), R(2), C(5, 2), // 8 Notes
+	R(2), C(4, 2), R(2), Eb(4, 2), R(2), C(4, 2), D(4, 2), Eb(4, 2), // 8 Notes
+	G(4, 2), R(2), C(5, 2), D(5, 2), R(2), G(4, 2), R(2), B(4, 2), // 8 Notes
+	R(2), G(4, 2), R(2), Eb(4, 2), R(2), G(4, 2), F_(4, 2), Eb(4, 2), // 8 Notes
 
-  C(4, 2), R(2), G(4, 2), Eb(4, 2), R(2), C(4, 2), R(2), D(4, 2), // 8 Notes
-  R(2), C(4, 2), R(2), Eb(4, 2), R(2), C(4, 2), D(4, 2), Eb(4, 2), // 8 Notes
-  G(4, 2), R(2), Eb(4, 2), G(4, 2), R(2), Eb(4, 2), R(2), G(4, 2), // 8 Notes
-  R(2), Eb(4, 2), R(2), Bb(4, 3), G(4, 3), F_(4, 3), Eb(4, 3), D(4, 2), Eb(4, 2), D(4, 2), // 10 Notes
+	C(4, 2), R(2), G(4, 2), Eb(4, 2), R(2), C(4, 2), R(2), D(4, 2), // 8 Notes
+  	R(2), C(4, 2), R(2), Eb(4, 2), R(2), C(4, 2), D(4, 2), Eb(4, 2), // 8 Notes
+  	G(4, 2), R(2), Eb(4, 2), G(4, 2), R(2), Eb(4, 2), R(2), G(4, 2), // 8 Notes
+  	R(2), Eb(4, 2), R(2), Bb(4, 3), G(4, 3), F_(4, 3), Eb(4, 3), D(4, 2), Eb(4, 2), D(4, 2), // 10 Notes
 };
 
-COMPOSE_SHIFTED(vitality_notes, Vitality, play_vitality, 300, 66, -1)
+COMPOSE(vitality_notes, Vitality, play_vitality, 300, 66)
 
 const double industry_baby_notes[83][2] = {
-  D(3, 3), R(3), tD(3, 3, 1), tR(3, 1), tD(3, 3, 1), tR(3, 1), tD(3, 3, 1), tR(3, 1), D(3, 3), R(3), D(3, 3), R(3), // 12 Notes
-  A(3, 3), R(3), tA(3, 3, 1), tR(3, 1), tA(3, 3, 1), tR(3, 1), tA(3, 3, 1), tR(3, 1), A(3, 3), R(3), A(3, 3), R(3), // 12 Notes
-  D(4, 3), R(3), tD(4, 3, 1), tR(3, 1), tD(4, 3, 1), tR(3, 1), tD(4, 3, 1), tR(3, 1), D(4, 3), R(3), D(4, 3), R(3), // 12 Notes
-  D(4, 1), D(4, 2), R(2), // 3 Notes
+  	D(3, 3), R(3), tD(3, 3, 1), tR(3, 1), tD(3, 3, 1), tR(3, 1), tD(3, 3, 1), tR(3, 1), D(3, 3), R(3), D(3, 3), R(3), // 12 Notes
+  	A(3, 3), R(3), tA(3, 3, 1), tR(3, 1), tA(3, 3, 1), tR(3, 1), tA(3, 3, 1), tR(3, 1), A(3, 3), R(3), A(3, 3), R(3), // 12 Notes
+  	D(4, 3), R(3), tD(4, 3, 1), tR(3, 1), tD(4, 3, 1), tR(3, 1), tD(4, 3, 1), tR(3, 1), D(4, 3), R(3), D(4, 3), R(3), // 12 Notes
+  	D(4, 1), D(4, 2), R(2), // 3 Notes
 
-  A(3, 3), G(3, 3), F_(3, 1), G(3, 3), F_(3, 3), // 5 Notes
-  E(3, 2), E(3, 3), R(3), E(3, 4), R(4), E(3, 4), R(4), E(3, 4), R(4), E(3, 4), R(4), // 11 Notes
-  F_(3, 3), E(3, 3), D(3, 1), D(3, 2), // 4 Notes
-  R(2), D(3, 2), E(3, 2), F_(3, 2), // 4 Notes
+  	A(3, 3), G(3, 3), F_(3, 1), G(3, 3), F_(3, 3), // 5 Notes
+  	E(3, 2), E(3, 3), R(3), E(3, 4), R(4), E(3, 4), R(4), E(3, 4), R(4), E(3, 4), R(4), // 11 Notes
+  	F_(3, 3), E(3, 3), D(3, 1), D(3, 2), // 4 Notes
+  	R(2), D(3, 2), E(3, 2), F_(3, 2), // 4 Notes
 
-  A(3, 3), G(3, 3), F_(3, 1), G(3, 3), F_(3, 3), // 5 Notes
-  E(3, 2), E(3, 3), R(3), E(3, 4), R(4), E(3, 4), R(4), E(3, 4), R(4), E(3, 4), R(4), // 11 Notes
-  F_(3, 3), E(3, 3), D(3, 1), D(3, 2), // 4 Notes
+  	A(3, 3), G(3, 3), F_(3, 1), G(3, 3), F_(3, 3), // 5 Notes
+  	E(3, 2), E(3, 3), R(3), E(3, 4), R(4), E(3, 4), R(4), E(3, 4), R(4), E(3, 4), R(4), // 11 Notes
+  	F_(3, 3), E(3, 3), D(3, 1), D(3, 2), // 4 Notes
 };
 
 COMPOSE(industry_baby_notes, Industry_Baby, play_industry_baby, 145, 83)
 
-const double bad_apple_notes[66][2] = {
-  Eb(3, 3), F_(3, 3), Gb(3, 3), Ab(3, 3), Bb(3, 2), Eb(4, 3), Db(4, 3), // 7 Notes
-  Bb(3, 2), Eb(3, 2), Bb(3, 3), Ab(3, 3), Gb(3, 3), F_(3, 3), // 6 Notes
-  Eb(3, 3), F_(3, 3), Gb(3, 3), Ab(3, 3), Bb(3, 2), Ab(3, 3), Gb(3, 3), // 7 Notes
-  F_(3, 3), Eb(3, 3), F_(3, 3), Gb(3, 3), F_(3, 3), Eb(3, 3), D(3, 3), F_(3, 3), // 8 Notes
-  Eb(3, 3), F_(3, 3), Gb(3, 3), Ab(3, 3), Bb(3, 2), Eb(4, 3), Db(4, 3), // 7 Notes
-  Bb(3, 2), Eb(3, 2), Bb(3, 3), Ab(3, 3), Gb(3, 3), F_(3, 3), // 6 Notes
-  Eb(3, 3), F_(3, 3), Gb(3, 3), Ab(3, 3), Bb(3, 2), Ab(3, 3), Gb(3, 3), // 7 Notes
-  Eb(3, 2), Gb(3, 2), Ab(3, 2), Bb(3, 2), // 4 Notes
-
-  Db(4, 3), Eb(4, 3), Bb(3, 3), Ab(3, 3), Bb(3, 2), Ab(3, 3), Bb(3, 3), // 7 Notes
-  Db(4, 3), Eb(4, 3), Bb(3, 3), Ab(3, 3), Bb(3, 2), Ab(3, 3), Bb(3, 3), // 7.3333 Notes
+const double bad_apple_notes[52][2] = {
+  	Eb(3, 3), F_(3, 3), Gb(3, 3), Ab(3, 3), Bb(3, 2), Eb(4, 3), Db(4, 3), // 7 Notes
+  	Bb(3, 2), Eb(3, 2), Bb(3, 3), Ab(3, 3), Gb(3, 3), F_(3, 3), // 6 Notes
+  	Eb(3, 3), F_(3, 3), Gb(3, 3), Ab(3, 3), Bb(3, 2), Ab(3, 3), Gb(3, 3), // 7 Notes
+  	F_(3, 3), Eb(3, 3), F_(3, 3), Gb(3, 3), F_(3, 3), Eb(3, 3), D(3, 3), F_(3, 3), // 8 Notes
+  	Eb(3, 3), F_(3, 3), Gb(3, 3), Ab(3, 3), Bb(3, 2), Eb(4, 3), Db(4, 3), // 7 Notes
+  	Bb(3, 2), Eb(3, 2), Bb(3, 3), Ab(3, 3), Gb(3, 3), F_(3, 3), // 6 Notes
+  	Eb(3, 3), F_(3, 3), Gb(3, 3), Ab(3, 3), Bb(3, 2), Ab(3, 3), Gb(3, 3), // 7 Notes
+  	F_(3, 2), Gb(3, 2), Ab(3, 2), Bb(3, 2), // 4 Notes
 };
 
-COMPOSE(bad_apple_notes, Bad_Apple, play_bad_apple, 138, 66)
+COMPOSE(bad_apple_notes, Bad_Apple, play_bad_apple, 138, 52)
+
+const double bad_apple_intro_notes[132][2] = {
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	Gb(4, 3), Eb(4, 4), Gb(4, 4), Ab(4, 3), Gb(4, 4), Ab(4, 4), // 6 Notes
+
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	Ab(4, 3), Gb(4, 4), Ab(4, 4), Gb(4, 3), Eb(4, 4), Gb(4, 4), // 6 Notes
+
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	Gb(4, 3), Eb(4, 4), Gb(4, 4), Ab(4, 3), Gb(4, 4), Ab(4, 4), // 6 Notes
+
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+ 	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	Eb(3, 4), Eb(3, 5), R(5), Eb(3, 4), Eb(4, 4), R(4), Eb(4, 4), Db(4, 4), Eb(4, 4), // 9 Notes
+  	tAb(4, 2, 1), tGb(4, 2, 1), tAb(4, 2, 1), tGb(4, 2, 1), tEb(4, 2, 1), tGb(4, 2, 1), // 6 Notes
+};
+
+COMPOSE(bad_apple_intro_notes, Bad_Apple_Intro, play_bad_apple_intro, 138, 132)
+
+const double bad_apple_bridge_notes[55][2] = {
+  	Db(4, 3), Eb(4, 3), Bb(3, 3), Ab(3, 3), Bb(3, 2), Ab(3, 3), Bb(3, 3), // 7 Notes
+  	Db(4, 3), Eb(4, 3), Bb(3, 3), Ab(3, 3), Bb(3, 2), Ab(3, 3), Bb(3, 3), // 7 Notes
+  	Ab(3, 3), Gb(3, 3), F_(3, 3), Db(3, 3), Eb(3, 2), Db(3, 3), Eb(3, 3), // 7 Notes
+  	F_(3, 3), Gb(3, 3), Ab(3, 3), Bb(3, 3), Eb(3, 2), Bb(3, 3), Db(4, 3), // 7 Notes
+  	Db(4, 3), Eb(4, 3), Bb(3, 3), Ab(3, 3), Bb(3, 2), Ab(3, 3), Bb(3, 3), // 7 Notes
+  	Db(4, 3), Eb(4, 3), Bb(3, 3), Ab(3, 3), Bb(3, 2), Eb(4, 3), F_(4, 3), // 7 Notes
+  	Gb(4, 3), F_(4, 3), Eb(4, 3), Db(4, 3), Bb(3, 2), Ab(3, 3), Bb(3, 3), // 7 Notes
+  	Ab(3, 3), Gb(3, 3), F_(3, 3), Db(3, 3), Eb(3, 2), R(2), // 6 Notes
+};
+
+COMPOSE_SHIFTED(bad_apple_bridge_notes, Bad_Apple_Bridge, play_bad_apple_bridge, 138, 55, 1)
+
+
+
+// —— End —— //
+
+
 
 #endif
